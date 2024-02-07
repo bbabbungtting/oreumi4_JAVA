7acbad9d5cbd721fccedb776f539fa62be2c5c6a
Jaewon <choijawon99@gmail.com>
2024-02-07 23:31:40 +0900

    [FEAT] 0207 dailyQuiz

--- src/FirstMain.java
+++ src/FirstMain.java
@@ -1,14 +1,18 @@
-import chapter7.Item;
-
 import java.util.*;
 
 public class FirstMain {
     public static void main(String[] args) {
-        ArrayList<Item> itemList;
+        int start_num = 3;
+        int end_num = 10;
 
+        int gap = end_num - start_num;
+
+        int[] answer = new int[gap + 1];
+
+        for (int i = 0; i <= gap; i++) {
+            answer[i] = i + start_num;
+        }
 
 
     }
-
-
 }
