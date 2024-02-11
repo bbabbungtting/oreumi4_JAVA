1d4c8b8f11cad5a2bca2be50efb4efd543776508
Jaewon <choijawon99@gmail.com>
2024-02-11 16:30:39 +0900

    [DONE] Application.java 완성

--- src/weeklyquiz/week3/Application.java
+++ src/weeklyquiz/week3/Application.java
@@ -2,6 +2,8 @@ package weeklyquiz.week3;
 
 public class Application {
     public static void main(String[] args) {
+        Controller controller = new Controller();
 
+        controller.start();
     }
 }
