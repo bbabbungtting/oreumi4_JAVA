4475404b165ae6607b8bbb369ce282539252d121
Jaewon <choijawon99@gmail.com>
2024-02-21 16:32:58 +0900

    [FEAT] stream 학습

--- src/chapter13/AscendingSort.java
+++ src/chapter13/AscendingSort.java
@@ -1,15 +1,38 @@
 package chapter13;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class AscendingSort {
     public int[] solution(int[] array1, int[] array2) {
         int[] result = new int[array1.length + array2.length];
 
         // 코드 작성
+        /*
+        int resultIndex = 0;
+        for (int i = 0; i < array1.length; i++) {
+            result[resultIndex] = array1[i];
+            resultIndex++;
+        }
+        for (int i = 0; i < array2.length; i++) {
+            result[resultIndex] = array2[i];
+            resultIndex++;
+        }
 
+        Arrays.sort(result);
+
+         */
+        result = IntStream.concat(Arrays.stream(array1), Arrays.stream(array2))
+                .sorted()
+                .toArray();
+
+
+        List<Integer> list = new ArrayList();
+        list.stream()
+                .filter(e -> e % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .max()
+                .ifPresent(System.out::println);
 
         return result;
     }
